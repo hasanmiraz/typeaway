0b87c9c6062251ad334198a34a1393c68aa0732f
miraz <hasibul.miraz@northsouth.edu>
2022-09-08 13:51:56 +0600

    generated string for typing

--- source/main.py
+++ source/main.py
@@ -1,15 +1,36 @@
+from cProfile import label
+from turtle import left
 from files.wordlist import word
 from tkinter import *
 from tkinter import ttk
 import random
-from time import sleep
+
+def get_random_word():
+    return str(random.choice(word))
+
+def get_20_random_word():
+    return random.sample(word, 20)
+
+def loop_word(word_list=[]):
+    if len(word_list)==0:
+        word_list = get_20_random_word()
+    else:
+        word_list.append(get_random_word())
+    print(len(word_list))
+    line = ""
+    line = " ".join(word_list)
+
+    print(word_list)
+    label.config(text=line)
+    window.after(500, loop_word, word_list)
 
 window = Tk()
+window.geometry("1000x600")
+frame = Frame(window, width=800, height=200, bg="yellow")
+frame.pack()
+label = Label(frame, wraplength=700)
 
-while( True):
-    random_word = random.choice(word)
-    print(random_word)
-    sleep(2)
-lable = Label(window, text=random_word)
-lable.pack()
+label.pack(side=LEFT)
+loop_word()
+
 window.mainloop()
