edd37205bf7f1b710002a428e6a7cdcce6a2b3bf
miraz <hasibul.miraz@northsouth.edu>
2022-09-09 19:15:33 +0600

    making the input

--- source/main.py
+++ source/main.py
@@ -11,26 +11,44 @@ def get_random_word():
 def get_20_random_word():
     return random.sample(word, 20)
 
-def loop_word(word_list=[]):
-    if len(word_list)==0:
-        word_list = get_20_random_word()
-    else:
-        word_list.append(get_random_word())
-    print(len(word_list))
-    line = ""
-    line = " ".join(word_list)
+#adding words
+# def loop_word(word_list=[]):
+#     if len(word_list)==0:
+#         word_list = get_20_random_word()
+#     else:
+#         word_list.append(get_random_word())
+#     print(len(word_list))
+#     line = ""
+#     line = " ".join(word_list)
 
-    print(word_list)
-    label.config(text=line)
-    window.after(500, loop_word, word_list)
+#     print(word_list)
+#     label.config(text=line)
+    # window.after(500, loop_word, word_list)
+
+def string_coloring():
+    pass
+
+def key_pressed(event):
+    w=Label(window,text="Key Pressed:"+event.char)
+    w.place(x= 500, y= 400)
+
+
 
 window = Tk()
 window.geometry("1000x600")
-frame = Frame(window, width=800, height=200, bg="yellow")
-frame.pack()
-label = Label(frame, wraplength=700)
 
-label.pack(side=LEFT)
-loop_word()
+line_list = get_20_random_word()
+letter_list = list(line_list[0])
+line_list.pop(0)
+print(letter_list)
+line_string = " ".join(line_list)
+label = Label(window, text= line_string, wraplength= 700)
+label.pack(side= TOP)
+
+#adding words
+# loop_word()
+
+#key press event
+window.bind("<Key>",key_pressed)
 
 window.mainloop()
